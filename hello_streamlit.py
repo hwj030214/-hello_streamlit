92a4b56cd4aa8b0877557b1c6b545d8a8f7e927c
hwj030214 <hwj030214@gmail.com>
2025-11-11 17:30:38 +0900

    hello_streamlit.py

--- hello_streamlit.py
+++ hello_streamlit.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.header('st.button')
 
 if st.button('Press'):
-     st.write('Hello I'm Woojin Hwang')
+     st.write("Hello I'm Woojin Hwang")
 else:
+
      st.write('Who am I?')
